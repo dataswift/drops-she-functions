4bfe7d6090b9250a74b637877e3c54c8da0ebfbc
Terry Lee <terry.lee@hatdex.org>
2018-12-21 12:32:02 +0000

    Word Cloud Visualiser@

--- lambda-word-cloud-visualiser/handler.py
+++ lambda-word-cloud-visualiser/handler.py
@@ -1,24 +1,10 @@
 import json
-
-
+import base64
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": event['body']
     }
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
---
+++ lambda-word-cloud-visualiser/word-cloud-visualiser.py
@@ -0,0 +1,37 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+import base64
+import json
+
+def visualise(event, context):
+    body_data = base64.b64decode(event['body'])
+    decoded_body = json.loads(body_data)['counts']
+    frequencies = {d['keyWord']: d['count'] for d in decoded_body}
+
+    wordcloud = WordCloud(background_color='white', max_font_size=40, margin=0).generate_from_frequencies(frequencies)
+    figure = plt.figure()
+    frame = plt.gca()
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+
+    frame.axes.get_xaxis().set_visible(False)
+    frame.axes.get_yaxis().set_visible(False)
+
+    from io import BytesIO
+    figfile = BytesIO()
+    figure.savefig(figfile, format='png', bbox_inches='tight', pad_inches=0)
+    figfile.seek(0)
+
+    data = base64.b64encode(figfile.getvalue()).decode('utf-8')
+
+    response = {
+        "isBase64Encoded": True,
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "image/png"
+        },
+        "body": data
+    }
+
+    return response
